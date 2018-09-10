8948bbfb638688786e8354e0d04edc8e1f099d95
Nima Bidari <nima.bidari@Y22-Design-Nima-Bidari.local>
2018-09-10 12:12:38 -0400

    Taking Inputs

---
+++ TakingInputs.py
@@ -0,0 +1,24 @@
+
+
+#Input
+print("Hello")
+name = input("What is your name: ")
+print("I will guess a number")
+random = float(input("Enter a number between 1 and 2: "))
+
+
+
+#Process
+if random > 2: 
+	nameOne == name + " isn't such a good person."
+
+else:
+	nameOne == name + "is a good person."
+
+
+#Output
+
+print(nameOne)
+
+
+print("PROGRAM END")

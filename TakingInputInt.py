29bce14cd36de13f40a0913add0cc7f568e61b60
Nima Bidari <nima.bidari@Y22-Design-Nima-Bidari.local>
2018-09-12 13:58:42 -0400

    update

--- TakingInputInt.py
+++ TakingInputInt.py
@@ -1,9 +1,20 @@
 #Input
 #Assignment Statement
 
-r = input("What is the radius?: ")
-h = input("What is the height?: ")
+r = int(input("What is the radius?: "))
+
+h = int(input("What is the height?: "))
+
+#Casting is the process of changing type
+#Strings - collections of characters
+#Int - integers
+#Float - numbers with decimals
 
 #Process
 
+sa = 2 * 3.14 * r * h + 2 * 3.14 * r*r
+sa = str(sa)
+
 #Output
+
+print("The surface area of your cylinder is " + sa + ".")

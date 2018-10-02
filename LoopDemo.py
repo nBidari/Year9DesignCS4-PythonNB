3547fed8e321cc083167730d4cc6ad8a429ccc8a
Nima Bidari <nima.bidari@Y22-Design-Nima-Bidari.local>
2018-10-02 11:15:14 -0400

    update + Loop Demo

---
+++ LoopDemo.py
@@ -0,0 +1,24 @@
+#A lop is a a programming structure that can repeat a section of code
+
+#There are two borad categories of loops
+# Conditional Loops (while) : As long as condition is true
+# Counted Loops (for) : uses counter of how many times loop is run.
+#
+# You can use any loop in any situaton but usually form a 
+# design erspective there is a better loop in terms of coding
+#
+#If you know in advanece how many times a loop shoudn run
+#a counter loop is probably a better choice
+#
+#
+#
+#If you dont know how many times a loop should run
+#a conditional loop is probably a better choice
+
+
+print("*****************************************************")
+
+word = input("Please input a word larger than 5 letters: ")
+
+print(word + " is a seriously long word!")
+
